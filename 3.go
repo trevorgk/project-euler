1282da861fd837414be461746820c72e6fc02525
Trevor Kilvington <trevorgk@gmail.com>
2014-09-25 13:44:39 +1000

    completed problem 3

--- 3.go
+++ 3.go
@@ -3,18 +3,33 @@ package main
 import "fmt"
 import "math"
 
-func sumOfDivisors(n int) int {
-	sum := 1
-	bound := int(math.Sqrt(float64(n)))
-	for i:= 2; i<= bound + 1; i++ {
-		if (n % i == 0) {
-			sum = sum + i + n/i
+func largestPrimeDivisor(n int) int {
+	divisors := getDivisors(n)
+	largestPrime := 0
+	for _, value := range divisors {
+		if len(getDivisors(value)) == 2 {
+			if largestPrime < value {
+				largestPrime = value
+			}
 		}
 	}
-	return sum
+	return largestPrime
+}
+
+func getDivisors(n int) []int {
+	bound := int(math.Sqrt(float64(n)))
+	divisors := make([]int,0)
+	divisors = append(divisors, 1)
+	for i:= 2; i <= bound + 1; i++ {
+		if (n % i == 0) {
+			divisors = append(divisors, i, n/i)
+		}
+	}
+	divisors = append(divisors, n)
+	return divisors
 }
 
 func main() {
 
-	fmt.Printf("result is: %d",sumOfDivisors(13195))
+	fmt.Printf("result is: %d", largestPrimeDivisor(600851475143))
 }
