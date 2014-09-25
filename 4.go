64a1b9ed01acdfb525b9eef3683993b739e87123
Trevor Kilvington <trevorgk@gmail.com>
2014-09-26 02:27:05 +1000

    completed problem 4

--- 4.go
+++ 4.go
@@ -5,6 +5,11 @@ import (
 	"strconv"
 )
 
+type Tuple struct {
+	x int
+	y int
+}
+
 func IsPalindrome(s string) bool {
 	reverse := Reverse(s)
 	return reverse == s
@@ -18,13 +23,20 @@ func Reverse(s string) string  {
     return string(runes)
 }
 func main() {
+	factors := make([]Tuple,0)
+	max := Tuple{0,0}
+	
 	for i := 999;i >= 100; i-- {
 		for j := i; j >= 100; j-- {
 			product := i * j
 			if (IsPalindrome(strconv.Itoa(product))) {
-				fmt.Println(product, "is a palindrome comprised of", i, "and", j)
-				return
+				//fmt.Println(product, "is a palindrome comprised of", i, "and", j)
+				factors = append(factors, Tuple{i,j})
+				if (i + j) > (max.x + max.y) {
+					max = Tuple {i, j}
+				}
 			}
 		}
 	}
+	fmt.Println("highest pair are:", max.x, "and", max.y,"which result in palindrome:", max.x * max.y)
 }
