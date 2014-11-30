738ff211bcdcff15693160f4166f12e40048eaba
Trevor Kilvington <trevorgk@gmail.com>
2014-12-01 02:56:22 +1100

    refactored lib into manageable pieces

--- 16.go
+++ 16.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"math"
-	//"math/big"
+	//"math"
+	"math/big"
 	//"strconv"
 )
 
 //	What is the sum of the digits of the number 2^n
-func euler_16(n float64) {
-	pow := math.Pow(2,n)
-	fmt.Println("2**i yields", pow)//, ([]byte(pow)))
+func euler_16(n int) {
+	pow := big.NewInt(1 << uint(n))
+	fmt.Printf("2**%v yields %v\n",n, pow)//, ([]byte(pow)))
+	//fmt.Println(pow)//, ([]byte(pow)))
 }
