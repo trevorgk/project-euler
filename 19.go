1e8e8f851801a93b3b7f9b1c53dbe8b05a9ecf28
Trevor Kilvington <trevorgk@gmail.com>
2014-11-30 14:45:29 +1100

    problem 19 close to finished

--- 19.go
+++ 19.go
@@ -8,20 +8,37 @@ import (
 
 func euler_19() {
 
-	if _ , err := eulerlib.DateIsA(time.Monday, 31, 2, 2000); err != nil{
-		fmt.Println(err)
-	}
+	//if _ , err := eulerlib.DateIsA(time.Monday, 31, 2, 2000); err != nil{
+	//	fmt.Println(err)
+	//}
 
-	sundaysOnTheFirst,// day, month,
-		year := 0,// 1, time.January,
-		1900
+	sundaysOnTheFirst, 
+		day, month, year := 0, 7, time.January, 1900
 
-	for i := 0;;i++ {
+	
+	for {
+		day += 7
 		if (year > 2000) {
 			break;
 		}
-
-
+		
+		daysInMonth := eulerlib.DaysInMonth(month, year)
+		
+		if (day % daysInMonth) > 0 {
+			day -= daysInMonth
+			if (day == 1) {
+				sundaysOnTheFirst++
+				fmt.Println(day, month, year, "is a Sunday")
+			}
+			
+			month += 1
+			if (month == time.December) {
+				month = time.January
+				year++
+			} else {
+				month++
+			}
+		}
 	}
 
 	fmt.Println("Sundays on the first:", sundaysOnTheFirst)
