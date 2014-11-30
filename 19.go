fcb3142d5bd9bf794e0094fdec7a85056e46ef6d
Trevor Kilvington <trevorgk@gmail.com>
2014-12-01 01:34:15 +1100

    using go's very handy built in date types - LOC reduced greatly

--- 19.go
+++ 19.go
@@ -6,13 +6,30 @@ import (
 	"time"
 )
 
+//	Unlike vanilla solution below, this func uses go's very handy built in - LOC reduced greatly
 func euler_19() {
+
+	sundaysOnTheFirst, date, week := 0, time.Date(1900, time.January, 7, 0, 0, 0, 0, time.UTC), time.Hour * 24 * 7
+
+	for ; date.Year() <= 2000; date = date.Add(week) {
+		if date.Day() == 1 && date.Year() >= 1901 {
+			sundaysOnTheFirst++
+		}
+		//fmt.Println("date is:", date)
+	}
+		
+	fmt.Println("Sundays on the first:", sundaysOnTheFirst)
+}
+
+//	solution implemented using almost no built-ins (except constants) 
+func euler_19_vanilla() {
 
 	sundaysOnTheFirst, 
 		day, month, year := 0, 7, time.January, 1900
 
 	for {
 		day += 7
+
 		if (year > 2000) {
 			break;
 		}
@@ -31,14 +48,8 @@ func euler_19() {
 			} else {
 				month++
 			}
-
 			//fmt.Printf("date is %v/%v/%v\n", day, month, year)
 		}
-		//runcount := 0
-		//if (runcount < 25 && year >= 1901) {
-		//	runcount++
-		//	fmt.Printf("date is %v/%v/%v\n", day, month, year)
-		//}
 	}
 
 	fmt.Println("Sundays on the first:", sundaysOnTheFirst)
